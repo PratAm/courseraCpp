d4c84b400c402ea55b913c4f7b4e7d5d8c4cc9c6
PratAm <amarpratap.knit@gmail.com>
2013-10-19 06:07:21 -0700

    formatting

--- test.cpp
+++ test.cpp
@@ -6,30 +6,28 @@ using namespace std;
 // no of element to push in vector
 const int N = 40 ; 
 
+// funtion to calcuate sum of elements in vector container
 inline void sum(int &sum, const vector<int> &data)
 {
 	// const vector iterator for vector traversal
 	vector<int>::const_iterator it;
+	//loop through vector and add its element
 	for( it = data.begin(); it != data.end(); ++it)
 	{
 		sum = sum + *it;
 	}
-	
 }
 
 int main()
 {
 	int accum = 0;
 	vector<int> data;
-
 	for( int i = 0; i < N; ++i)
 	{
 		// push values to vector
 		data.push_back(i);
 	}
-  // call function to calculate sum
 	sum(accum, data);
-  // print final sum
 	cout<<"sum is " << accum <<endl;
 	return 0;
 }
